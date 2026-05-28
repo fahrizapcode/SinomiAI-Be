fd133b8201464dc2f5b60070223621bb4b5f2bc8
John Doe <madda.athiarahman@gmail.com>
2026-05-28 13:57:22 +0700

    setup configuration for deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import productRoutes from './routes/productRoutes.js';
 import generateRoutes from './routes/generateRoutes.js';
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
